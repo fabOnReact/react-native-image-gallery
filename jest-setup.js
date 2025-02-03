99a74704c9286c1f8f0f562ce1044a64db972179
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-02-03 15:57:55 +0800

    mock gesture handler for tests

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,11 @@
 import fetchMock from 'jest-fetch-mock';
+import 'react-native-gesture-handler/jestSetup';
 
 fetchMock.enableMocks();
+
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
+jest.mock('react-native-gesture-handler', () =>
+  require('react-native-gesture-handler/jestSetup'),
+);
