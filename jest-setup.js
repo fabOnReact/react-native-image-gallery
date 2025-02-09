adbb9e605b8e460905efef85f4b78813293449d0
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-02-09 11:12:49 +0800

    remove console messages in production and test environment

--- jest-setup.js
+++ jest-setup.js
@@ -2,6 +2,14 @@ import fetchMock from 'jest-fetch-mock';
 import 'react-native-gesture-handler/jestSetup';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+// Suppress console.log, console.warn, and console.error in test environment
+global.console = {
+  ...console,
+  log: jest.fn(), // Suppress console.log
+  warn: jest.fn(), // Suppress console.warn
+  error: jest.fn(), // Suppress console.error
+};
+
 fetchMock.enableMocks();
 
 jest.mock('react-native-reanimated', () =>
@@ -14,4 +22,7 @@ jest.mock('react-native-gesture-handler', () =>
 // Mock AsyncStorage
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
-jest.mock('@components/HearthWithLiquidButton', () => 'HeartWithLiquidButton');
+jest.mock(
+  './src/components/HearthWithLiquidButton',
+  () => 'HeartWithLiquidButton',
+);
--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,4 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|react-native-.*|@react-navigation/.*|@tanstack/react-query|@shopify/react-native-skia|react-native-gesture-handler|react-native-reanimated|d3|d3-array|d3-scale|d3-shape)/)',
   ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Ensure Babel handles transformations
-  },
-  moduleNameMapper: {
-    '^d3$': '<rootDir>/node_modules/d3/dist/d3.min.js', // Map d3 to its compiled version
-    '^@components/(.*)$': '<rootDir>/src/components/$1', // Ensure Jest resolves @components alias
-  },
 };
